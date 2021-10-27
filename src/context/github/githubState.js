d9539c26286d61d821e026df9e173b3c86484658
David Herbert <daviehert@gmail.com>
2021-10-27 02:26:07 +0100

    Implemented github context

---
+++ src/context/github/githubState.js
@@ -0,0 +1,22 @@
+import React, { useReducer } from "react";
+import githubContext from "./githubContext";
+import axios from "axios";
+import GithubReducer from "./githubReducer";
+import {
+  SEARCH_USERS,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+  SET_LOADING,
+} from "../types";
+
+const GithubState = (props) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer();
+};

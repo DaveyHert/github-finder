b0cd78ea7afc185d560572584c2ac1dbe1172529
David Herbert <daviehert@gmail.com>
2021-10-28 19:47:54 +0100

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, Fragment } from "react";
-import axios from "axios";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
@@ -7,14 +6,14 @@ import Users from "./components/users/Users";
 import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
-import about from "./components/pages/about";
+import About from "./components/pages/about";
 import GithubState from "./context/github/GithubState";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [users, setUsers] = useState([]);
+  // const [user, setUser] = useState({});
+  // const [repos, setRepos] = useState([]);
+
   const [alert, setAlert] = useState(null);
 
   //Set alert if input is empty
@@ -41,18 +40,17 @@ const App = () => {
               <Route
                 exact
                 path='/'
-                //a render props is used when multiple components are to be shown
                 render={(props) => (
                   <Fragment>
                     {/* Search component */}
                     <Search showAlert={showAlert} />
                     {/* Users list component */}
-                    <Users users={users} loading={loading} />
+                    <Users />
                   </Fragment>
                 )}
               />
               {/* About Page Route */}
-              <Route exact path='/about' component={about} />
+              <Route exact path='/about' component={About} />
               {/* Get single user route */}
               <Route exact path='/user/:login' component={User} />
             </Switch>
